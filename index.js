a0dfecd0018de4d2326b44b803c944ef81b2f71c
Vaishnavi Byjus <vaishnavibyjus@Shubhams-MacBook-Pro-2.local>
2023-07-26 22:23:30 +0400

    node js practice

---
+++ index.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+const readableStream = fs.createReadStream("./file.txt", { encoding : "utf-8", highWaterMark: 2});
+
+const writeableStream = fs.createWriteStream("./file2.txt");
+
+readableStream.on("data", (chunk) => {
+    console.log(chunk);
+    writeableStream.write(chunk);
+})
